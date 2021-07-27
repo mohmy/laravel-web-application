7ba4304758c470d7af378bc5c0704ff47686193e
Iskandar Ali <3722339+iskandarali@users.noreply.github.com>
2021-07-27 10:28:13 +0800

    Add users seeder
    Add a demo user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Demo La Nie',
+            'email' => 'demolanie3@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ]);
+
+        User::factory()
+            ->create();
+
+    }
+}
