4a4367b4d9cfd0287b5e772871bb2b684b810f3d
iskandarali <iskandarali@users.noreply.github.com>
2021-07-30 08:45:53 +0000

    Fix styling

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\AccountController;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AccountController;
 
 /*
 |--------------------------------------------------------------------------
